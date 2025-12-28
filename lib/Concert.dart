3e3710870b22fd19ddaaf0788f017d2866153b93
DoThanhLoc2334 <locdothanh233@gmail.com>
2025-12-28 08:01:36 +0700

    chore: create 2 model(Concert and Seat)

--- lib/Concert.dart
+++ lib/Concert.dart
@@ -1,6 +1,11 @@
 class Concert{
   int id;
   String name;
+  String location;
+  DateTime date;
+  int rows;
+  int columns;
+  List<Seat> seats;
 
   Concert({required this.id, required this.name});
 
--- lib/Seat.dart
+++ lib/Seat.dart
@@ -1,10 +1,6 @@
 class Seat {
-  int seatvertivalamount = 0;
-  int seathorizontalamount = 0;
-
-  Seat({required this.seathorizontalamount, required this.seatvertivalamount});
-  int GetSeatAmount()
-  {
-    return seatvertivalamount * seathorizontalamount;
-  }
+  int row;
+  int column;
+  bool isBooked;
+  bool isDisabled;
 }
